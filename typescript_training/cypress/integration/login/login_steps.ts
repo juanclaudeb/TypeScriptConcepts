fac1a8cab4bb0905fd998298e9bbfc563ef89215
juanclaudeb <40379760+juanclaudeb@users.noreply.github.com>
2021-11-25 09:55:51 +0200

    Remove comments from login step

--- typescript_training/cypress/integration/login/login_steps.ts
+++ typescript_training/cypress/integration/login/login_steps.ts
@@ -1,5 +1,4 @@
 import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
-//import { binding, given, then, when} from 'cucumber-tsflow';
 
 const url = 'https://google.com'
 Given('I am at the login page', () => {
